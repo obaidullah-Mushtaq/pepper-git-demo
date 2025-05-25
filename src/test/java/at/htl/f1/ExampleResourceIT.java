0175d4b2400b909d29a14a4148a92738bd46f5ea
Obaidullah Mushtaq <o.mushtaq@students.htl-leonding.ac.at>
2025-05-25 15:22:43 +0200

    Initial commit

---
+++ src/test/java/at/htl/f1/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package at.htl.f1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
